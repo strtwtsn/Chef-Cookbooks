e2706f0d4b61476003a0c13171c81cc68aa10b0d
Stuart Watson <stuart.watson@unep-wcmc.org>
2012-04-13 10:35:39 +0100

    Added basic GIS setup

--- cookbooks/postgis/recipes/default.rb
+++ cookbooks/postgis/recipes/default.rb
@@ -21,3 +21,16 @@ make
 make install
 EOH
 end
+
+bash "configure postgis" do
+  user "root"  
+  code <<-EOH    
+  createdb  -T template0 -O postgres -U postgres -E UTF8 template_postgis
+  createlang plpgsql -U postgres -d template_postgis
+  psql -d template_postgis -U postgres -f /usr/share/postgresql/8.4/contrib/postgis-1.5/postgis.sql
+  psql -d template_postgis -U postgres -f /usr/share/postgresql/8.4/contrib/postgis-1.5/spatial_ref_sys.sql
+  ldconfig
+  EOH
+  only_if { `psql -U postgres -t -c "select count(*) from pg_catalog.pg_database where datname = 'template_postgis'"`.include? '0'}
+end
+
