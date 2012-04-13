0ab367f072ba90ccd5f3a6391ad662faa92ecf06
Stuart Watson <stuart.watson@unep-wcmc.org>
2012-04-13 10:28:34 +0100

    added setup for access over EC2 IPs

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -13,3 +13,14 @@ apt-get update
 apt-get install -y postgresql-9.1 postgresql-server-dev-9.1
 EOH
 end
+
+# Allow access from EC2 IPs
+
+template "/etc/postgresql/9.1/main/pg_hba.conf" do
+  source "pg_hba.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode 0600
+  notifies :restart, resources(:service => "postgresql"), :immediately
+end
+
